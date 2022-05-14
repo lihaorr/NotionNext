45eac216f5f9db41a4a1f62db0f008b3f0559fe2
lihaorr <32298299+lihaorr@users.noreply.github.com>
2022-05-15 06:15:16 +0800

    Update LayoutIndex.js

--- themes/hexo/LayoutIndex.js
+++ themes/hexo/LayoutIndex.js
@@ -4,12 +4,8 @@ import BlogPostListScroll from './components/BlogPostListScroll'
 import Header from './components/Header'
 import CONFIG_HEXO from './config_hexo'
 import LayoutBase from './LayoutBase'
-import Link from 'next/link'
-import Card from './components/Card'
 
 export const LayoutIndex = (props) => {
-    const { categories } = props
-  const { locale } = useGlobal()
   return <LayoutBase {...props} headerSlot={CONFIG_HEXO.HOME_BANNER_ENABLE && <Header {...props} />}>
     {BLOG.POST_LIST_STYLE === 'page' ? <BlogPostListPage {...props} /> : <BlogPostListScroll {...props} />}
   </LayoutBase>
